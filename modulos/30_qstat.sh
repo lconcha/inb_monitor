6bc0144bab2c301f354af09f063051a4fc4e8b4b
Luis Concha <lconcha@gmail.com>
2024-07-19 19:57:17 -0600

    first

---
+++ modulos/30_qstat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+html=$1
+
+
+qstat -u "*" > tmp_qstat.txt
+
+sed 's/$/<br \/>/g' ./tmp_qstat.txt > tmp_qstat.html
+
+
+echo "<b>qstat <br /></b>"  >> $html
+echo "    <code>" >> $html
+cat tmp_qstat.html >> $html
+echo "    </code>" >> $html
