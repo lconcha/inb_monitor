2ad146437c948dc7b12f11e70784668bfb9b3b9d
Luis Concha <lconcha@gmail.com>
2024-07-22 12:15:28 -0600

    added comments throughtout

--- inb_monitor_make_html.sh
+++ inb_monitor_make_html.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+#
+# Make sure you have installed to-html through cargo (rust)
+# https://github.com/Aloso/to-html
+
+
+# Put to-html on the path
 function path_add() {
   rel_path_to_add=$1
   path_to_add=$(readlink -f $rel_path_to_add)
@@ -10,14 +17,16 @@ function path_add() {
     echolor red "[ERROR] Cannot add to PATH, directory does not exist: $path_to_add"
   fi
 }
-
-
 path_add /home/inb/lconcha/.cargo/bin
 
+
+# Define files
 html=/misc/penfield/lconcha/inb_monitor/index.html
 tmp_html=/misc/penfield/lconcha/inb_monitor/tmp_index.html
 here=$(dirname $0)
 
+
+# Begin cretion of html file
 echo "
 <html>
  <body>
@@ -27,6 +36,7 @@ echo "
 the_date=$(date)
 echo "<b> $the_date <br /><br /></b>"  >> $tmp_html
 
+# Run all available modules
 for f in ${here}/modulos/*.sh;
 do
   echo "Running $f"
@@ -34,10 +44,12 @@ do
   echo "<br />"  >> $tmp_html
 done
 
+# Finish up the html
 echo "
  </body>
 </html>
 " >> $tmp_html
 
+# Replace old html
 mv $tmp_html $html
 echo "Created $html"
